6abd2526a4debf313b5c6adf4d3ca8db76f10901
Александр <Cigurd186@icloud.com>
2023-03-04 22:20:18 +0300

    add keyword buttons add text

--- src/main/kotlin/EchoMain.kt
+++ src/main/kotlin/EchoMain.kt
@@ -1,43 +1,81 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
-import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
+import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
+import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
+import com.github.kotlintelegrambot.logging.LogLevel
 
 fun main() {
+
     val bot = bot {
+        //https://github.com/kotlin-telegram-bot/kotlin-telegram-bot/blob/main/docs/logging.md
+        logLevel = LogLevel.None
+        //todo читать из файла
         token = ""
 
         dispatch {
+
+
+            //TODO в отдельный метод
             text {
-                val button = InlineKeyboardMarkup.create(
-                    listOf(InlineKeyboardButton.CallbackData("Ping", "Pong"))
-                )
-                bot.sendMessage(
-                    chatId = ChatId.fromId(message.chat.id),
-                    text = text,
-                    replyMarkup = button
-                )
+                if ((text == humanResourceContacts
+                            || text == accountingContacts
+                            || text == paymentContacts
+                            || text == "/start").not()
+                ) {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = "Для получения информации введите команду /start",
+                    )
+                }
+
             }
+
+
             command("start") {
+                val keyboardMarkup = KeyboardReplyMarkup(
+                    keyboard = generateUsersButton(),
+                    resizeKeyboard = true
+                )
                 bot.sendMessage(
                     chatId = ChatId.fromId(message.chat.id),
-                    text = "Hello sweet!"
+                    //todo читать из файла
+                    text = """Добрый день! вы можете узнать:
+                         $humanResourceContacts, 
+                         $accountingContacts,
+                         $paymentContacts
+                    """.trimMargin(),
+                    replyMarkup = keyboardMarkup
                 )
-            }
-            callbackQuery("pong") {
-                Thread.sleep(5000)
-                bot.editMessageText(
-                    chatId = ChatId.fromId(callbackQuery.message!!.chat.id),
-                    messageId = callbackQuery.message!!.messageId,
-                    text = "Хуй",
-                )
+
+                text(humanResourceContacts) {
+                    bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Вот контакты")
+                }
+                text(accountingContacts) {
+                    bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Вот справки")
+                }
+                text(paymentContacts) {
+                    bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Ответы по заработной плате")
+                }
             }
         }
     }
 
+
     bot.startPolling()
+}
+
+
+const val humanResourceContacts = "Контакты отдела кадров"
+const val accountingContacts = "Заказать справку в бухгалтерии"
+const val paymentContacts = "Задать вопросы по заработной плате"
+
+
+fun generateUsersButton(): List<List<KeyboardButton>> {
+    return listOf(
+        listOf(KeyboardButton(humanResourceContacts), KeyboardButton(accountingContacts)),
+        listOf(KeyboardButton(paymentContacts))
+    )
 }
