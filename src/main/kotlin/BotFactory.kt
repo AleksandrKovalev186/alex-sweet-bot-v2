8a8980c77645bc630d66451dc14b7d6dc99ba9b5
Александр <Cigurd186@icloud.com>
2023-03-10 20:46:31 +0300

    fix logging fix double buttons

--- src/main/kotlin/BotFactory.kt
+++ src/main/kotlin/BotFactory.kt
@@ -9,28 +9,31 @@ import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
 import com.github.kotlintelegrambot.logging.LogLevel
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import java.time.LocalDateTime
 
 class BotFactory {
 
     private val humanResourceContacts = "Контакты отдела кадров"
     private val accountingContacts = "Заказать справку в бухгалтерии"
     private val paymentContacts = "Задать вопросы по заработной плате"
+    private val logFile = File("logs.txt")
+    private var localDateTime: LocalDateTime = LocalDateTime.now();
+
 
     val bot = bot {
-
-        //todo писать логи в файл
         //https://github.com/kotlin-telegram-bot/kotlin-telegram-bot/blob/main/docs/logging.md
-        logLevel = LogLevel.None
+        logLevel = LogLevel.Network.Body
 
-        //val filePath = "/app/logs/january.log"
-
-        val content = Files.readString(Paths.get("src/main/kotlin/token.txt"), Charsets.UTF_8)
+        val content = Files.readString(Paths.get("token.txt"), Charsets.UTF_8)
         token = content
 
         dispatch {
-            println("QET BOT IS LAUNCHED")
+
+            logFile.writeText("QET BOT IS LAUNCHED at $localDateTime", Charsets.UTF_8)
 
             text {
                 if (listOf(
@@ -40,12 +43,18 @@ class BotFactory {
                         "/start"
                     ).contains(text).not()
                 ) {
-                    bot.sendMessage(
+
+                    val startMessage = bot.sendMessage(
                         chatId = ChatId.fromId(message.chat.id),
-                        text = "Для получения информации введите команду /start",
+                        text = "Для получения информации введите команду /start"
+
+                    )
+                    Files.write(
+                        logFile.toPath(),
+                        "\n send /start info $startMessage".toByteArray(),
+                        StandardOpenOption.APPEND
                     )
                 }
-
             }
 
 
@@ -56,7 +65,6 @@ class BotFactory {
                 )
                 bot.sendMessage(
                     chatId = ChatId.fromId(message.chat.id),
-                    //todo читать из файла
                     text = """Добрый день! вы можете узнать:
                               $humanResourceContacts, 
                               $accountingContacts,
@@ -64,41 +72,60 @@ class BotFactory {
                     """.trimMargin(),
                     replyMarkup = keyboardMarkup
                 )
+            }
+            text(humanResourceContacts) {
 
-                text(humanResourceContacts) {
+                val humanResourceMessage =
                     bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Вот контакты")
-                }
-                text(accountingContacts) {
-                    val button = InlineKeyboardMarkup.create(
-                        listOf(
-                            InlineKeyboardButton.CallbackData("Справка 1", "Справка 1"),
-                            InlineKeyboardButton.CallbackData("Справка 2", "Справка 2")
-                        )
+
+                Files.write(
+                    logFile.toPath(),
+                    "\n send $humanResourceContacts $humanResourceMessage".toByteArray(),
+                    StandardOpenOption.APPEND
+                )
+
+            }
+            text(paymentContacts) {
+                val paymentContactMessage = bot.sendContact(
+                    chatId = ChatId.fromId(message.chat.id),
+                    "+79218671553",
+                    "Имя",
+                    "Фамилия"
+                )
+                Files.write(
+                    logFile.toPath(),
+                    "\n send $paymentContacts $paymentContactMessage".toByteArray(),
+                    StandardOpenOption.APPEND
+                )
+
+            }
+            text(accountingContacts) {
+                val button = InlineKeyboardMarkup.create(
+                    listOf(
+                        InlineKeyboardButton.CallbackData("Справка 1", "Справка 1"),
+                        InlineKeyboardButton.CallbackData("Справка 2", "Справка 2")
                     )
+                )
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Выберите необходимую справку",
+                    replyMarkup = button
+                )
+
+                callbackQuery("Справка 1") {
                     bot.sendMessage(
-                        chatId = ChatId.fromId(message.chat.id),
-                        text = "Выберите необходимую справку",
-                        replyMarkup = button
+                        chatId = ChatId.fromId(callbackQuery.message!!.chat.id),
+                        text = "Информация по первой справке",
                     )
-
-                    callbackQuery("Справка 1") {
-                        bot.sendMessage(
-                            chatId = ChatId.fromId(callbackQuery.message!!.chat.id),
-                            text = "Информация по первой справке",
-                        )
-                    }
-
-                    callbackQuery("Справка 2") {
-                        bot.sendMessage(
-                            chatId = ChatId.fromId(callbackQuery.message!!.chat.id),
-                            text = "Информация по второй справке",
-                        )
-                    }
                 }
-                text(paymentContacts) {
-                    bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Ответы по заработной плате")
 
+                callbackQuery("Справка 2") {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(callbackQuery.message!!.chat.id),
+                        text = "Информация по второй справке",
+                    )
                 }
+
             }
         }
 
@@ -112,3 +139,6 @@ class BotFactory {
     }
 
 }
+
+
+
